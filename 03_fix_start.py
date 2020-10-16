7374fcbc39ead0074416f7b7ea267a7ac57a35c4
Nicole <barrosnicole094@gmail.com>
2020-10-16 13:12:23 -0300

    Resolução segundo e terceiro desafio

--- 03_fix_start.py
+++ 03_fix_start.py
@@ -15,8 +15,8 @@ onde todas as instancias de stra foram substituidas por strb.
 
 def fix_start(s):
     # +++ SUA SOLUÇÃO +++
-    return
 
+    return s[0] + s[1:].replace(s[0], '*') if len(s) > 1 else s[0]
 
 # --- Daqui para baixo são apenas códigos auxiliáries de teste. ---
 
